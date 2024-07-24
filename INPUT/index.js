4cc375edea71df0715d99da8bf492c99435a285a
ChristoferNVR2 <157941358+ChristoferNVR2@users.noreply.github.com>
2024-07-24 10:29:23 -0500

    Update index.js

--- INPUT/index.js
+++ INPUT/index.js
@@ -7,3 +7,6 @@ document.getElementById("mySubmit").onclick = function() {
     username = document.getElementById("myText").value;
     document.getElementById("myH1").textContent = `Hello ${username}`;
 }
+
+// let name = window.prompt("What is your name?");
+// console.log(name);
