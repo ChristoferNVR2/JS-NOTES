2a9007fe0ea9952d240d2d84cb373b6b197492f0
ChristoferNVR2 <157941358+ChristoferNVR2@users.noreply.github.com>
2024-07-24 10:19:12 -0500

    Create index.js

---
+++ INPUT/index.js
@@ -0,0 +1,9 @@
+// ctrl + k + c -> comment
+// ctrl + F2 -> replace name
+
+let username;
+
+document.getElementById("mySubmit").onclick = function() {
+    username = document.getElementById("myText").value;
+    document.getElementById("myH1").textContent = `Hello ${username}`;
+}
