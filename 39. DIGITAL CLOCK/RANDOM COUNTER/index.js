0764ffa021f73290b79885ae43ff46d578d8a2f2
ChristoferNVR2 <157941358+ChristoferNVR2@users.noreply.github.com>
2024-09-08 19:52:30 -0500

    Create index.js

---
+++ 39. DIGITAL CLOCK/RANDOM COUNTER/index.js
@@ -0,0 +1,13 @@
+function updateClock() {
+    const now = new Date();
+    const hell = new Date(2024, 8, 9, 7, 0, 0, 0);
+    const diff = Math.abs(now - hell);
+    const hours = Math.floor(diff / (1000 * 60 * 60)).toString().padStart(2, '0');
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000).toString().padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").textContent = timeString;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
